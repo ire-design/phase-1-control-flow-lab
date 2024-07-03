9ce05eb1173a0ee23c3f33befdd969a5a480c5f7
ire-design <irenemwikalii04@gmail.com>
2024-07-03 21:28:28 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let message;
+
+  if (distance <= 400){
+    message = 'This one is on me!'
+  }else if ( distance > 400 && distance <= 2000  ){
+    message = 'That will be twenty bucks.'
+  }else if ( distance > 2000 && distance <= 2500){
+    message = 'I will gladly take your thirty bucks.'
+  }else if ( distance > 2500){
+    message = 'No can do.'
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+scuberGreetingForFeet (199);
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(cityName){
   // Write your code here!
+  let message;
+
+  if ( cityName === 'NYC') {
+    message = 'Ok, sounds good.' 
+  } else {
+    message = 'No go.'
+  }
+  return message;
 }
+ternaryCheckCity(NYC)
+
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+  let message;
+
+    if ( tip === 'generous'){
+      message = 'Thank you so much.'
+    }else if ( tip === 'not as generous'){
+      message = 'Thank you.'
+    }else {
+      message = 'Bye.'
+    }
+
+    return message;
+}
+switchOnCharmFromTip (generous)
